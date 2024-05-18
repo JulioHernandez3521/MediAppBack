3afe420d02635303fab05414fc29ba4a45f5af58
JulioHernandez <ju010970@gmail.com>
2024-05-18 14:30:26 -0600

    ajuste de nombre

--- src/main/java/com/mitocode/security/JwtResponse.java
+++ src/main/java/com/mitocode/security/JwtResponse.java
@@ -2,5 +2,5 @@ package com.mitocode.security;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record JwtResponse(@JsonProperty(value = "acces_token") String accessToken) {
+public record JwtResponse(@JsonProperty(value = "access_token") String accessToken) {
 }
